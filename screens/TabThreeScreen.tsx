51c0d388d647a88a066db8a21ef0741337faeb6d
Afshin Mokhtari <afshinator@hotmail.com>
2020-11-04 22:35:51 -0800

    Add the animated top tabs

--- screens/TabThreeScreen.tsx
+++ screens/TabThreeScreen.tsx
@@ -1,32 +1,235 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import * as React from "react";
+import { StyleSheet } from "react-native";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Animated,
+  ScrollView,
+  Image,
+  Dimensions,
+} from "react-native";
 
-export default function TabOneScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Three</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabThreeScreen.js" />
-    </View>
-  );
+const { width } = Dimensions.get("window");
+
+export default class App extends React.Component {
+  state = {
+    active: 0,
+    xTabOne: 0,
+    xTabTwo: 0,
+    translateX: new Animated.Value(0),
+    translateXTabOne: new Animated.Value(0),
+    translateXTabTwo: new Animated.Value(width),
+    translateY: -1000,
+  };
+
+  handleSlide = (type) => {
+    let {
+      active,
+      xTabOne,
+      xTabTwo,
+      translateX,
+      translateXTabOne,
+      translateXTabTwo,
+    } = this.state;
+    Animated.spring(translateX, {
+      toValue: type,
+      duration: 100,
+    }).start();
+    if (active === 0) {
+      Animated.parallel([
+        Animated.spring(translateXTabOne, {
+          toValue: 0,
+          duration: 100,
+        }).start(),
+        Animated.spring(translateXTabTwo, {
+          toValue: width,
+          duration: 100,
+        }).start(),
+      ]);
+    } else {
+      Animated.parallel([
+        Animated.spring(translateXTabOne, {
+          toValue: -width,
+          duration: 100,
+        }).start(),
+        Animated.spring(translateXTabTwo, {
+          toValue: 0,
+          duration: 100,
+        }).start(),
+      ]);
+    }
+  };
+
+  render() {
+    let {
+      xTabOne,
+      xTabTwo,
+      translateX,
+      active,
+      translateXTabOne,
+      translateXTabTwo,
+      translateY,
+    } = this.state;
+    return (
+      <View style={{ flex: 1 }}>
+        <View style={styles.fullSizeish}>
+          <View style={styles.tabLayout}>
+            <Animated.View
+              style={[styles.slider, {
+                transform: [
+                  {
+                    translateX,
+                  },
+                ],
+              }]}
+            />
+            <TouchableOpacity
+              style={styles.leftTouchable}
+              onLayout={(event) =>
+                this.setState({
+                  xTabOne: event.nativeEvent.layout.x,
+                })
+              }
+              onPress={() =>
+                this.setState({ active: 0 }, () => this.handleSlide(xTabOne))
+              }
+            >
+              <Text
+                style={{
+                  color: active === 0 ? "#fff" : "#007aff",
+                }}
+              >
+                Tab One
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.rightTouchable}
+              onLayout={(event) =>
+                this.setState({
+                  xTabTwo: event.nativeEvent.layout.x,
+                })
+              }
+              onPress={() =>
+                this.setState({ active: 1 }, () => this.handleSlide(xTabTwo))
+              }
+            >
+              <Text
+                style={{
+                  color: active === 1 ? "#fff" : "#007aff",
+                }}
+              >
+                Tab Two
+              </Text>
+            </TouchableOpacity>
+          </View>
+
+          <ScrollView>
+            <Animated.View
+              style={{
+                justifyContent: "center",
+                alignItems: "center",
+                transform: [
+                  {
+                    translateX: translateXTabOne,
+                  },
+                ],
+              }}
+              onLayout={(event) =>
+                this.setState({
+                  translateY: event.nativeEvent.layout.height,
+                })
+              }
+            >
+              <Text>Greetings Earthlings</Text>
+              <View style={{ marginTop: 20 }}>
+                <Image
+                  source={require("../assets/images/img1.jpg")}
+                  style={{
+                    width: 300,
+                    height: 300,
+                    borderRadius: 15,
+                  }}
+                />
+              </View>
+            </Animated.View>
+
+            <Animated.View
+              style={{
+                justifyContent: "center",
+                alignItems: "center",
+                transform: [
+                  {
+                    translateX: translateXTabTwo,
+                  },
+                  {
+                    translateY: -translateY,
+                  },
+                ],
+              }}
+            >
+              <Text>Take me to your leader</Text>
+              <View style={{ marginTop: 20 }}>
+                <Image
+                  source={require("../assets/images/img2.jpg")}
+                  style={{
+                    width: 300,
+                    height: 300,
+                    borderRadius: 15,
+                  }}
+                />
+              </View>
+            </Animated.View>
+          </ScrollView>
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  fullSizeish: {
+    width: "90%",
+    marginLeft: "auto",
+    marginRight: "auto",
+  },
+  tabLayout: {
+    flexDirection: "row",
+    marginTop: 40,
+    marginBottom: 20,
+    height: 36,
+    position: "relative",
+  },
+  leftTouchable: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: "#007aff",
+    borderRadius: 4,
+    borderRightWidth: 0,
+    borderTopRightRadius: 0,
+    borderBottomRightRadius: 0,
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  rightTouchable: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: "#007aff",
+    borderRadius: 4,
+    borderLeftWidth: 0,
+    borderTopLeftRadius: 0,
+    borderBottomLeftRadius: 0,
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+  slider: {
+    position: "absolute",
+    width: "50%",
+    height: "100%",
+    top: 0,
+    left: 0,
+    backgroundColor: "#007aff",
+    borderRadius: 4,
+  }
 });
