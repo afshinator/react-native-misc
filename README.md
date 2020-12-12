8e02dd5d4c53e15613cf889268106d10e979daef
Afshin Mokhtari <afshinator@hotmail.com>
2020-12-11 21:34:04 -0800

    add baseline snapchat clone code

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # react-native-misc
 Some examples and proof of concepts and such in one place so I don't have to make a million repo's.
+
+### Tab 1 - in progress
+
+### Tab 2 - Animated Clock
+
+### Tab 3 - Animated Slider
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,32 +1,74 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import * as React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { StyleSheet } from "react-native";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import EditScreenInfo from "../components/EditScreenInfo";
+import { Text, View } from "../components/Themed";
+
+import Snapchat, { stories } from "./../snapchat/Snapchat";
+import { SnapchatRoutes } from "../snapchat/Model";
+// import Story from './../snapchat/Story';
+import StoryComp from "./../snapchat/Story";
+
+export const assets = stories
+  .map((story) => [story.avatar, story.source])
+  .flat();
+
+const Stack = createStackNavigator<SnapchatRoutes>();
+const Navigator = () => (
+  <Stack.Navigator
+    screenOptions={{
+      gestureEnabled: false,
+      headerShown: false,
+      cardOverlayEnabled: true,
+      cardStyle: { backgroundColor: "transparent" },
+    }}
+    mode="modal"
+  >
+    <Stack.Screen name="Snapchat" component={Snapchat} />
+    <Stack.Screen name="Story" component={StoryComp} />
+  </Stack.Navigator>
+);
 
 export default function TabOneScreen() {
+  /*
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>Tab 1 - Snapchat clone</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.js" />
     </View>
+  );
+  */
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        gestureEnabled: false,
+        headerShown: false,
+        cardOverlayEnabled: true,
+        cardStyle: { backgroundColor: "transparent" },
+      }}
+      mode="modal"
+    >
+      <Stack.Screen name="Snapchat" component={Snapchat} />
+      <Stack.Screen name="Story" component={StoryComp} />
+    </Stack.Navigator>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
